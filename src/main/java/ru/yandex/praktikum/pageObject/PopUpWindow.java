34b5cda44d47255365d729db6b049297795d1c1f
Ermakova Natalia <natalysolitonsf@yandex.com>
2024-06-23 00:18:07 +0300

    sprint 4 project's files added

---
+++ src/main/java/ru/yandex/praktikum/pageObject/PopUpWindow.java
@@ -0,0 +1,33 @@
+package ru.yandex.praktikum.pageObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+//Страница https://qa-scooter.praktikum-services.ru/order После оформления заказа появится окно (popup) -Заказ оформлен
+
+public class PopUpWindow {
+    WebDriver driver;
+    private final By popUpHeaderAfterCreateOrder = By.xpath(".//div[text()='Заказ оформлен']");
+    private final By buttonYes = By.xpath(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM' and text()='Да']");
+
+    public PopUpWindow(WebDriver driver) {
+
+        this.driver = driver;
+    }
+
+    public void clickButtonYes() {
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(buttonYes)).click();
+    }
+
+    public String getHeaderAfterCreateOrder() {
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(driver -> (driver.findElement(popUpHeaderAfterCreateOrder).getText() != null
+                && !driver.findElement(popUpHeaderAfterCreateOrder).getText().isEmpty()
+        ));
+        return driver.findElement(popUpHeaderAfterCreateOrder).getText();
+    }
+}
